475a916f95a21699715cca64687c23706791905b
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-10-08 16:58:55 -0700

    get project id done

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,20 @@
 // add middlewares here related to projects
+
+const projects = require("./projects-model");
+
+function validProjectId() {
+  return (req, res, next) => {
+    projects.get(req.params.id).then((pro) => {
+      if (pro) {
+        req.pro = pro;
+        next();
+      } else {
+        res.status(404).json({
+          message: "project not found",
+        });
+      }
+    });
+  };
+}
+
+module.exports = { validProjectId };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,7 +1,8 @@
 // Write your "projects" router here!
 const express = require("express");
+const { validProjectId } = require("./projects-middleware");
 const router = express.Router();
-const projects = require('./projects-model')
+const projects = require("./projects-model");
 
 router.get("/", (req, res, next) => {
   projects
@@ -10,6 +11,10 @@ router.get("/", (req, res, next) => {
       res.status(200).json(pro);
     })
     .catch(next);
+});
+
+router.get("/:id", validProjectId(), (req, res) => {
+  res.status(200).json(req.pro);
 });
 
 module.exports = router;
