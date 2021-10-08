fffd0aedc1b1665834ffc8a2d834ca6dbd22803e
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-10-08 16:18:17 -0700

    server running

--- api/server.js
+++ api/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json())
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: "Something went nwrong, please try again!"
+    })
+})
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: "Hello Chevy!" })
+})
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
